5ba4728db1570ab0ff44e4c4b01544f3123ae11d
Misha Redchych <redchych2@gmail.com>
2022-10-24 21:21:02 +0300

    Add project files.

---
+++ Services/AppDataService.cs
@@ -0,0 +1,34 @@
+﻿using SafeMessenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeMessenge.Services;
+
+public class AppDataService
+{
+    public ApiService ApiService;
+    public List<User> Users { get; set; } = new();
+    public User? CurrentUser { get; set; }
+    public List<Chat> Chats { get; set; } = new();
+
+    public AppDataService(ApiService apiService)
+    {
+        ApiService = apiService;
+
+    }
+
+    public List<User> SetUsersFromApi()
+    {
+        Users = ApiService.ReadAll<User>("users").ToList();
+        return Users;
+    }
+
+    public List<Chat> SetUserChatsFromApi(long userId)
+    {
+        Chats = ApiService.ReadAll<Chat>($"chats?users_id_array={userId}").ToList();
+        return Chats;
+    }
+}
